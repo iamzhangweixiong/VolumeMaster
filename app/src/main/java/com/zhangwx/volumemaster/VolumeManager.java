5251cd0d8d8d4c8c8237516d56bc94a467d8b6c5
zhangwxgo@gmail.com <Zwx19930301>
2017-04-13 11:04:39 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangwx/volumemaster/VolumeManager.java
@@ -0,0 +1,16 @@
+package com.zhangwx.volumemaster;
+
+/**
+ * Created by zhangwx on 2017/4/13.
+ */
+
+public class VolumeManager {
+
+    public VolumeManager() {
+
+    }
+
+    public void resetVolume() {
+
+    }
+}
